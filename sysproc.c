7d2cdbb0edd07012fc230a1534ac65b3257904be
arv1ndh <arvindhc@uci.edu>
2018-11-22 20:23:08 -0800

    Added functionality to return the contents of the user stack registers

--- sysproc.c
+++ sysproc.c
@@ -94,5 +94,20 @@ sys_uptime(void)
 int
 sys_backtrace(void)
 {
+    struct trapframe* tf = myproc()->tf;
+    cprintf("edi: 0x%x\n", tf->edi);
+    cprintf("esi: 0x%x\n", tf->esi);
+    cprintf("ebp: 0x%x\n", tf->ebp);
+    cprintf("ebx: 0x%x\n", tf->ebx);
+    cprintf("edx: 0x%x\n", tf->edx);
+    cprintf("ecx: 0x%x\n", tf->ecx);
+    cprintf("eax: 0x%x\n", tf->eax);
+    cprintf("gs: 0x%x\n", tf->gs);
+    cprintf("fs: 0x%x\n", tf->fs);
+    cprintf("es: 0x%x\n", tf->es);
+    cprintf("ds: 0x%x\n", tf->ds);
+    cprintf("cs: 0x%x\n", tf->cs);
+    cprintf("eip: 0x%x\n", tf->eip);
+    cprintf("eflags: 0x%x",tf->eflags);
     return 1;
 }
