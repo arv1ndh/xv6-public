e20c0eb26b3f25c92e345058e27c29d659d0b193
arv1ndh <arvindhc@uci.edu>
2018-11-24 17:58:54 -0800

    Made few display changes

--- ps.c
+++ ps.c
@@ -11,8 +11,8 @@ main(int argc, char *argv[])
     int b = getprocinfo(0,&p); 
     if (b != -1) {
         printf(1, "\tID\tP_ID\tSZ\tC_W\tK\tName\tState\n");
-        printf(1, "\t%d\t%d\t%d\t%d\t%d\t%s\t%s\n", p.pid, p.parent_pid, p.sz, p.channel_wait,p.killed,p.name, p_states[p.state]);
-        printf(1, "\n|LEGEND|\nID    --> Process ID\nP_ID  --> Process ID of Parent if present else 0\nSZ    --> Process Size\nC_W   --> 1 if Process is waiting on channel 0 otherwise\nK     --> 1 if the process has been killed\nName  --> Name of the process\nState --> State of the process\n\n");
+        printf(1, "\t%d\t%d\t%d\t%s\t%s\t%s\t%s\n", p.pid, p.parent_pid, p.sz, p.channel_wait == 0 ? "No":"Yes",p.killed == 0 ? "No": "Yes",p.name, p_states[p.state]);
+        printf(1, "\n|LEGEND|\nID    --> Process ID\nP_ID  --> Process ID of Parent if present else 0\nSZ    --> Process Size\nC_W   --> Is the Process waiting on channel?\nK     --> Is the Process killed?\nName  --> Name of the process\nState --> State of the process\n\n");
         //printf(1, "Process size: %d\n", p.sz);
         //printf(1, "CHannel Wait: %d\n", p.channel_wait);
         //printf(1, "Killed: %d\n", p.killed);
