522155f02126a0a7ee62b0f995c9181213ad6a55
arv1ndh <arvindhc@uci.edu>
2018-12-11 15:34:44 -0800

    used mutex lock in a different function

--- thread.c
+++ thread.c
@@ -88,12 +88,33 @@ void do_work(void *arg){
     return;
 }
 
+void mutex_do_work(void *arg){
+    int i; 
+    int old;
+   
+    struct balance *b = (struct balance*) arg; 
+    printf(1, "Starting do_work: s:%s\n", b->name);
+
+    for (i = 0; i < b->amount; i++) { 
+         thread_mutex_lock(&mlock);
+         old = total_balance;
+         delay(100000);
+         total_balance = old + 1;
+         thread_mutex_unlock(&mlock);
+         thread_spin_unlock(&lock);
+    }
+  
+    printf(1, "Done s:%x\n", b->name);
+
+    thread_exit();
+    return;
+}
+
 int main(int argc, char *argv[]) {
 
   struct balance b1 = {"b1", 3200};
   struct balance b2 = {"b2", 2800};
   thread_spin_init(&lock);
-  //thread_mutex_init(&mlock);
  
   void *s1, *s2;
   int t1, t2, r1, r2;
@@ -101,6 +122,7 @@ int main(int argc, char *argv[]) {
   s1 = malloc(4096);
   s2 = malloc(4096);
 
+  printf(1, "Using Spin Lock\n");
   t1 = thread_create(do_work,(void*)&b1,s1);
   t2 = thread_create(do_work, (void*)&b2, s2); 
 
@@ -110,5 +132,17 @@ int main(int argc, char *argv[]) {
   printf(1, "Threads finished: (%d):%d, (%d):%d, shared balance:%d\n", 
       t1, r1, t2, r2, total_balance);
 
+  printf(1, "Using Mutex Lock\n");
+  thread_mutex_init(&mlock);
+  total_balance = 0;
+
+  t1 = thread_create(mutex_do_work,(void*)&b1,s1);
+  t2 = thread_create(mutex_do_work, (void*)&b2, s2); 
+
+  r1 = thread_join();
+  r2 = thread_join();
+  printf(1, "Threads finished: (%d):%d, (%d):%d, shared balance:%d\n", 
+      t1, r1, t2, r2, total_balance);
+  
   exit();
 }
